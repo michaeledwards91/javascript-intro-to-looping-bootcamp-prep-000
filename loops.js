d9f425990dd0675f2868025c3c2f03f705b7471c
Michael Edwards <michael.edwards1991@gmail.com>
2016-09-12 03:55:50 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push( `I am ${i} strange loop${i===1 ? ' ' : 's'}` );
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.console.log(n);
+    n -= 1;
+  }
+  return 'done';
+}
+
+function doWhile(array) {
+
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+
+  return array;
+}
